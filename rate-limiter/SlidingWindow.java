aca8eb5ce981d60d24e8ee488bc4a1c4998d6ca5
a6dulaleem <abdul.aleem1@dell.com>
2022-10-20 22:20:08 -0700

    Added rate limiter

---
+++ rate-limiter/SlidingWindow.java
@@ -0,0 +1,41 @@
+import java.util.Queue;
+
+public class SlidingWindow implements RateLimiter {
+    Queue<Integer> slidingWindow;
+    int bucketCap;
+    int time;
+
+    public SlidingWindow(int time, int cap) {
+        this.time = time;
+        this.bucketCap = cap;
+    }
+
+    @Override
+    public boolean grantAccess() {
+        // update the queue
+
+        long currentTime = System.currentTimeMillis();
+        updateQueue(currentTime);
+
+        // check if queue is overflowed
+
+        if (slidingWindow.size() < bucketCap) {
+            slidingWindow.offer((int) currentTime);
+            return true;
+        }
+        return false;
+    }
+
+    private void updateQueue(long currentTime) {
+        if (slidingWindow.isEmpty())
+            return;
+        long time = (currentTime - slidingWindow.peek()) / 1000; // because time is in ms dividing by 1000
+
+        while (time >= this.time) {
+            slidingWindow.poll();
+            if (slidingWindow.isEmpty())
+                break;
+            time = (currentTime - slidingWindow.peek()) / 1000;
+        }
+    }
+}
