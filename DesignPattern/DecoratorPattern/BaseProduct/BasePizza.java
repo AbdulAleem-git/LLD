adbe17a4dc697ccc4ee767608686c2cbd56963b6
abdulaleem-git <zah7131@gmail.com>
2022-12-26 21:32:23 -0800

    Added Decorator Pattern

---
+++ DesignPattern/DecoratorPattern/BaseProduct/BasePizza.java
@@ -0,0 +1,5 @@
+package BaseProduct;
+
+public abstract class BasePizza {
+    public abstract int cost();
+}
---
+++ DesignPattern/DecoratorPattern/BaseProduct/Farmhouse.java
@@ -0,0 +1,10 @@
+package BaseProduct;
+
+public class Farmhouse extends BasePizza {
+
+    @Override
+    public int cost() {
+        return 200;
+    }
+    
+}
