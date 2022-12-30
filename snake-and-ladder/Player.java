54f7158b5f3c515322991f5ac4c269cb0d55eb5b
abdulaleem-git <zah7131@gmail.com>
2022-12-29 22:03:30 -0800

    Added snake and ladder

---
+++ snake-and-ladder/Player.java
@@ -0,0 +1,8 @@
+public class Player {
+    int currentPosition;
+    String Id;
+    Player(int currentPosition , String Id){
+        this.currentPosition = currentPosition;
+        this.Id = Id;
+    }
+}
